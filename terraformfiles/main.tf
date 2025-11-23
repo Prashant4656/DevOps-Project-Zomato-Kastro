034ec728dfc324f9e9cc734a33d68f8502c004a1
Prashant4656 <prashantwadi@gmail.com>
2025-11-23 17:03:44 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,28 +1,22 @@
 variable "pem_file" {
-  description = "Path to SSH private key"
+  description = "PEM file path from Jenkins"
   type        = string
 }
+
 resource "aws_instance" "test-server" {
-  ami = "ami-0fa3fe0fa7920f68e"
-  instance_type = "t3.small"
-  key_name = "bookmyshow"
-  vpc_security_group_ids = ["sg-05eb5a556e628a155"]
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = "bookmyshow"  # your EC2 key pair (must already exist)
+
   connection {
-     type = "ssh"
-     user = "ec2-user"
-     private_key = file(var.pem_file)
-     host = self.public_ip
-     }
-  provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(var.pem_file)
+    host        = self.public_ip
   }
-  tags = {
-     Name = "test-server"
-     }
+
   provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/zomatoapp/terraformfiles/ansiblebook.yml"
-     }
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ansiblebook.yml -i '${self.public_ip},' --private-key ${var.pem_file}"
   }
+}
+
